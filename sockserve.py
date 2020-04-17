2b270483461ec12e9d6e1d60d4863f25ca79359c
Zachary Bilmen <zb3qk@virginia.edu>
2020-04-16 20:14:41 -0400

    push

--- sockserve.py
+++ sockserve.py
@@ -12,7 +12,7 @@ async def hello(websocket, path):
 		    await websocket.send(greeting)
 		    print(f"> {greeting}")
 
-start_server = websockets.serve(hello, "localhost", 8080)
+start_server = websockets.serve(hello, "ws://192.168.1.180", 8080)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
