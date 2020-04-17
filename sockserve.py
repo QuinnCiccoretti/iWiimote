ce73db485b0dc88fd9401b71d70022d8a76d5940
Zachary Bilmen <zb3qk@virginia.edu>
2020-04-16 20:41:15 -0400

    Nothun

--- sockserve.py
+++ sockserve.py
@@ -1,18 +1,26 @@
 import asyncio
 import websockets
+import socket
 
 async def hello(websocket, path):
 	while True:
-	    name = await websocket.recv()
-	    if name:
-		    print(f"< {name}")
+		name = await websocket.recv()
+		count = 70
+		if name:
+			print(f"< {name}")
 
-		    greeting = f"Hello {name}!"
+			greeting = f"Hello {name}!"
 
-		    await websocket.send(greeting)
-		    print(f"> {greeting}")
+			await websocket.send(greeting)
+			print(f"> {greeting}")
 
-start_server = websockets.serve(hello, "ws://192.168.1.180", 8080)
+			if count <= 0:
+				break
+			count -= 1
+
+ipAddr = socket.gethostbyname(socket.gethostname())
+print(ipAddr)
+start_server = websockets.serve(hello, ipAddr , 12000)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
