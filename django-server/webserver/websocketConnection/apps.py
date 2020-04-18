8c64575f9428251e29064379416daa88aedde973
QuinnCiccoretti <quinnc423@gmail.com>
2020-04-18 13:38:54 -0400

    add a django site

--- django-server/webserver/websocketConnection/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebsocketconnectionConfig(AppConfig):
-    name = 'websocketConnection'
--- django-server/webserver/websocketConnection/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'), # request lobby ID here
-    path('instance/<int:lobbyId>/', views.lobby, name='lobby'),
-]
