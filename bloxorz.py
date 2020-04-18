2de3a7ac78cb8c01aabb4c709e2be6db9270ff7c
QuinnCiccoretti <quinnc423@gmail.com>
2020-04-18 17:41:42 -0400

    bloxorz code

---
+++ bloxorz.py
@@ -0,0 +1,54 @@
+import requests
+import keyboard
+import mouse
+
+
+#phyphox configuration
+PP_ADDRESS = "http://192.168.1.232"
+PP_CHANNELS = ["accX", "accY", "accZ"]
+LOCK_INPUT = False
+
+def update(url):
+	data = requests.get(url=url).json()
+	accX = data["buffer"][PP_CHANNELS[0]]["buffer"][0]
+	accY = data["buffer"][PP_CHANNELS[1]]["buffer"][0]
+	accZ = data["buffer"][PP_CHANNELS[2]]["buffer"][0]
+	global LOCK_INPUT
+	# mouse.move(-accX*20, -accY*20, absolute=False)
+	threshold = 3
+	if(accX and accY and accZ):
+		#print(accX, sep=" ")
+		left = accY < -threshold
+		right = accY > threshold
+		up = accX < -threshold
+		down = accX > threshold
+		any_input = up or down or left or right
+		if(not LOCK_INPUT):
+			if(left):
+				print("Left")
+				keyboard.press_and_release('left')
+			if(right):
+				print("Right")
+				keyboard.press_and_release('right')
+
+			if(up):
+				print("up")
+				keyboard.press_and_release('up')
+			
+			if(down):
+				print("Down")
+				keyboard.press_and_release('down')
+			if(any_input):
+				LOCK_INPUT = True
+		elif(not any_input):
+			LOCK_INPUT=False
+
+			
+def main():
+	url = PP_ADDRESS + "/get?" + ("&".join(PP_CHANNELS))
+	while True:
+		update(url)
+		
+
+if __name__ == '__main__':
+	main()
