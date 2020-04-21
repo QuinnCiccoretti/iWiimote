703972b2775535cec7b43fcd24061c0316bd2d6c
QuinnCiccoretti <quinnciccoretti@gmail.com>
2020-04-20 23:21:49 -0400

    implement mario controls

---
+++ mario.py
@@ -0,0 +1,50 @@
+import keyboard
+import mouse
+from iWiimote import iWiimote
+
+#phyphox configuration
+LOCK_INPUT = False
+MOVE_DIR = "left"
+PP_CHANNELS = ["accX", "accY", "accZ"]
+def update(iw):
+    data = iw.getData()
+    accX = data["accX"]
+    accY = data["accY"]
+    accZ = data["accZ"]
+    
+    global LOCK_INPUT
+    global MOVE_DIR
+    threshold = 3
+    
+    if(accX and accY and accZ):
+        left = accY < -threshold
+        right = accY > threshold
+        up = accX < -threshold
+        down = accX > threshold
+        any_input = up or down or left or right
+        if(not LOCK_INPUT):
+            move_dir = None
+            if(left):
+                move_dir = "left"
+            if(right):
+                move_dir = "right"
+            if(up):
+                move_dir = "up"			
+            if(down):
+                move_dir = "down"
+            if(any_input):
+                keyboard.press(move_dir)
+                LOCK_INPUT = True
+        elif(not any_input):
+                LOCK_INPUT=False
+                for direc in ["up","down", "left", "right"]:
+                    keyboard.release(direc)
+
+			
+def main():
+    iw = iWiimote(PP_CHANNELS)
+    while True:
+        update(iw)
+
+if __name__ == '__main__':
+    main()
