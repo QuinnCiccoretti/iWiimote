8ff4e8ee96468d5d447008537da2c0e0da38e636
Zachary Bilmen <zb3qk@virginia.edu>
2020-04-21 01:15:32 -0400

    Added QR Code support in full. QR code leads to a webpage that has instructions for setup with links to the Certification files for ios, the website to give permissions on android, and a link with a query specifiying the IP address to the heroku website. This is intended to be merged with Quinn's Python Fire implementation

--- QRCodeTest.py
+++ QRCodeTest.py
@@ -7,6 +7,10 @@ import pyqrcode
 from pyqrcode import QRCode
 from PIL import Image
 
+import os
+import sys
+import fileinput
+
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(('10.255.255.255', 1))
 # hostname = socket.gethostname()    
@@ -15,7 +19,7 @@ s.close()
 IP = socket.gethostbyname(hostname)      
 print("Your Computer IP Address is:" + IP)
 
-url1 = IP + ":" + "8000"
+url1 = "http://"+IP + ":" + "8000"
 
 #Generate QR Code
 url = pyqrcode.create(url1)
@@ -25,6 +29,20 @@ img = cv2.imread("myqr.png")
 cv2.imwrite('myqr.png',img)
 im = Image.open('myqr.png')
 im.show()
+
+
+with open("index_source.html") as f:
+    with open("index.html", "w") as f1:
+        for line in f:
+            f1.write(line)
+
+# Inserts the IP address into the correct positions in the index.html file
+text_to_search = "(IP)" 
+replacement_text = IP+":12000"
+with fileinput.FileInput("index.html", inplace=True, backup='.bak') as file:
+    for line in file:
+        print(line.replace(text_to_search, replacement_text), end='')
+
 
 #Start http server
 Handler = http.server.SimpleHTTPRequestHandler
