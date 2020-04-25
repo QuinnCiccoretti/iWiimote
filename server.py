8a452e080862acd144bcd89ce589ccf623e97d46
Zachary Bilmen <zb3qk@virginia.edu>
2020-04-25 13:34:26 -0400

    local serverchange

--- server.py
+++ server.py
@@ -37,7 +37,7 @@ localhost_pem = pathlib.Path(__file__).with_name("iwiimote_server.cer")
 ssl_context.load_cert_chain(localhost_pem, keyfile="iwiimote_server.key")
 print("Server is running!")
 
-start_server = websockets.serve(hello, "192.168.1.234", 12000, ssl=ssl_context)
+start_server = websockets.serve(hello, "192.168.1.3", 12000, ssl=ssl_context)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
